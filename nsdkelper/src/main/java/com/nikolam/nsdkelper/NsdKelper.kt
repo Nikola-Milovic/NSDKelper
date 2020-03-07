c24b8e5bc1832828e8764a3264b0f2644031086e
Nikola Milovic <nikolamilovic2001@gmail.com>
2020-03-07 11:36:18 +0100

    preparing for jitpack

--- nsdkelper/src/main/java/com/nikolam/nsdkelper/NsdKelper.kt
+++ nsdkelper/src/main/java/com/nikolam/nsdkelper/NsdKelper.kt
@@ -1,2 +1,13 @@
 package com.nikolam.nsdkelper
 
+import android.net.nsd.NsdManager
+import android.util.Log
+
+open class NsdKelper(private val ndsManager: NsdManager) {
+
+    fun test(){
+        Log.d("TAG", "FRUTALISIOUS")
+    }
+
+}
+
